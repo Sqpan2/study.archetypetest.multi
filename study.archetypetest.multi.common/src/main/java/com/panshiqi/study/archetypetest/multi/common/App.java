2cc780dfb54af2cfcfb7e3e5376323c3892ff185
Sqpan2 <951514058@qq.com>
2018-04-08 23:39:42 +0800

    自定义maven骨架demo--多模块项目
    自定义maven骨架demo--多模块项目

---
+++ study.archetypetest.multi.common/src/main/java/com/panshiqi/study/archetypetest/multi/common/App.java
@@ -0,0 +1,13 @@
+package com.panshiqi.study.archetypetest.multi.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
